b7cfbdac23792d202db682399aaae332fe4eba9b
Thomas Piergiovanni <thomas.piergiovanni@outlook.fr>
2021-02-10 07:05:56 +0100

    learning django tuto

--- sondage_project/polls/views.py
+++ sondage_project/polls/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 
 from django.http import HttpResponse, Http404
 from django.template import loader
@@ -18,14 +18,26 @@ def index(request):
 
 
 def detail(request, question_id):
-    try:
-        question = Question.objects.get(pk=question_id)
-        context = {
-                'question': question}
-    except Question.DoesNotExist:
-        raise Http404("Question does not exist")
+    ### v1
+    # question = Question.objects.get(pk=question_id)
     # return HttpResponse("Your're looking at question %s." % question_id)
-    return render(request,'polls/detail.html', context)
+
+
+    ### v2
+    # try:
+    #     question = Question.objects.get(pk=question_id)
+    #     context = {
+    #             'question': question}
+    # except Question.DoesNotExist:
+    #     raise Http404("Question does not exist")
+    # return render(request,'polls/detail.html', context)
+
+
+    ### v3
+    question = get_object_or_404(Question, pk=question_id)
+    context = {
+        'question': question}
+    return render(request, 'polls/detail.html', context)
 
 
 def results(request, question_id):
@@ -36,4 +48,3 @@ def results(request, question_id):
 def vote(request, question_id):
     return HttpResponse("You're voting on question %s." % question_id)
 
-## TO test
