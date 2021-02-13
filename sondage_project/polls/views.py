811ea9b0da241f81120e0a86517aae59c9107778
Thomas Piergiovanni <thomas.piergiovanni@outlook.fr>
2021-02-13 13:49:16 +0100

    finished django off tutorial part5

--- sondage_project/polls/views.py
+++ sondage_project/polls/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.template import loader
 from django.urls import reverse
+from django.utils import timezone
 from django.views import generic
 
 from .models import Choice, Question
@@ -15,9 +16,17 @@ class IndexView(generic.ListView):
     context_object_name = 'latest_question_list'
 
     def get_queryset(self):
-        """Return the last five published questions.
+        ### v4
+        # """Return the last five published questions.
+        # """
+        # return Question.objects.order_by('-pub_date')[:5]
+
+        ### v5
+        """ Return the last five published questions (
+            not including those set to be published in the future).
         """
-        return Question.objects.order_by('-pub_date')[:5]
+        return Question.objects.filter(
+                pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
 
 
 class DetailView(generic.DetailView):
