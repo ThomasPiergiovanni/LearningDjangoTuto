b5480ba27af988c6f434098c6b782eabbb3f26d5
Thomas Piergiovanni <thomas.piergiovanni@outlook.fr>
2021-02-15 06:33:52 +0100

    working on django off tutorial part 7

--- sondage_project/polls/admin.py
+++ sondage_project/polls/admin.py
@@ -1,8 +1,19 @@
 from django.contrib import admin
 
 # Register your models here.
-from django.contrib import admin
+from .models import Question, Choice
 
-from .models import Question
 
-admin.site.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+
+    ### v1
+    # fields = ['pub_date', 'question_text']
+
+    ### v2
+    fieldsets = [
+        (None,      {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date']}),
+    ]
+
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice)
