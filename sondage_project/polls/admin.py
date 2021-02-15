272c2bb07dd00f2a8b199a23c9be68ff2fd9520e
Thomas Piergiovanni <thomas.piergiovanni@outlook.fr>
2021-02-15 06:59:49 +0100

    working on django off tutto part 7

--- sondage_project/polls/admin.py
+++ sondage_project/polls/admin.py
@@ -31,6 +31,9 @@ class QuestionAdmin(admin.ModelAdmin):
         ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
     ]
     inlines = [ChoiceInline]
+    
+    ### v5
+    list_display = ('question_text','pub_date', 'was_published_recently')
 
 
 admin.site.register(Question, QuestionAdmin)
