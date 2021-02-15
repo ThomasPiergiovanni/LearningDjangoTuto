19fa18a719dacc664e686019108d123188fe8798
Thomas Piergiovanni <thomas.piergiovanni@outlook.fr>
2021-02-15 06:50:54 +0100

    working on django official tutto. part 7

--- sondage_project/polls/admin.py
+++ sondage_project/polls/admin.py
@@ -1,8 +1,18 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Question, Choice
+from .models import Choice, Question
 
+
+### v3
+# class ChoiceInline(admin.StackedInline):
+#     model = Choice
+#     extra = 3
+
+### v4
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
 
 class QuestionAdmin(admin.ModelAdmin):
 
@@ -10,10 +20,20 @@ class QuestionAdmin(admin.ModelAdmin):
     # fields = ['pub_date', 'question_text']
 
     ### v2
+    # fieldsets = [
+    #     (None,      {'fields': ['question_text']}),
+    #     ('Date information', {'fields': ['pub_date']}),
+    # ]
+
+    ### v3
     fieldsets = [
         (None,      {'fields': ['question_text']}),
-        ('Date information', {'fields': ['pub_date']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
     ]
+    inlines = [ChoiceInline]
+
 
 admin.site.register(Question, QuestionAdmin)
-admin.site.register(Choice)
+
+### v2
+# admin.site.register(Choice)
