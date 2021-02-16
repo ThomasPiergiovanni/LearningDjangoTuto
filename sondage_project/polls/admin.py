3dcfa9d4ee49b1f0c10aa6125201a36b5f977e0e
Thomas Piergiovanni <thomas.piergiovanni@outlook.fr>
2021-02-16 06:51:20 +0100

    working on django lesson

--- sondage_project/polls/admin.py
+++ sondage_project/polls/admin.py
@@ -26,15 +26,38 @@ class QuestionAdmin(admin.ModelAdmin):
     # ]
 
     ### v3
+    # fieldsets = [
+    #     (None,      {'fields': ['question_text']}),
+    #     ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    # ]
+    # inlines = [ChoiceInline]
+    
+    ### v5
+    # fieldsets = [
+    #     (None,      {'fields': ['question_text']}),
+    #     ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    # ]
+    # inlines = [ChoiceInline]
+    # list_display = ('question_text','pub_date', 'was_published_recently')
+
+    ### v6
+    # fieldsets = [
+    #     (None,      {'fields': ['question_text']}),
+    #     ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    # ]
+    # inlines = [ChoiceInline]
+    # list_display = ('question_text','pub_date', 'was_published_recently')
+    # list_filter = ['pub_date']
+
+    ### v7
     fieldsets = [
         (None,      {'fields': ['question_text']}),
         ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
     ]
     inlines = [ChoiceInline]
-    
-    ### v5
     list_display = ('question_text','pub_date', 'was_published_recently')
-
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
 
 admin.site.register(Question, QuestionAdmin)
 
--- sondage_project/polls/models.py
+++ sondage_project/polls/models.py
@@ -16,9 +16,18 @@ class Question(models.Model):
     #     return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
     
     ### v2
+    # def was_published_recently(self):
+    #     now = timezone.now()
+    #     return now - datetime.timedelta(days=1) <= self.pub_date <= now
+
+    ### v3
     def was_published_recently(self):
         now = timezone.now()
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
+
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published recently?'
 
 
 class Choice(models.Model):
